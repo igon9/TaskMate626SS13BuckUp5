1f7e2f5a798c17493c0df48cad870e9d4b8e4b15
igon9 <igonpogi9@gmail.com>
2026-06-30 14:18:28 +0900

    Remove MapperScan

--- src/main/java/com/example/taskmate/TaskMate626SS13BuckUp5Application.java
+++ src/main/java/com/example/taskmate/TaskMate626SS13BuckUp5Application.java
@@ -1,11 +1,9 @@
 package com.example.taskmate;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.taskmate.repository")
 public class TaskMate626SS13BuckUp5Application {
 
     public static void main(String[] args) {
