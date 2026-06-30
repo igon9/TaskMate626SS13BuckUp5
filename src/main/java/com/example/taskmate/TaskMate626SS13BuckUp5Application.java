6adfb4a0be6e7e8c91549fd19fc93b5cdf2efd21
igon9 <igonpogi9@gmail.com>
2026-06-30 12:58:25 +0900

    Add MyBatis mapper XML files

--- src/main/java/com/example/taskmate/TaskMate626SS13BuckUp5Application.java
+++ src/main/java/com/example/taskmate/TaskMate626SS13BuckUp5Application.java
@@ -1,13 +1,15 @@
 package com.example.taskmate;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.taskmate.repository")
 public class TaskMate626SS13BuckUp5Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskMate626SS13BuckUp5Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMate626SS13BuckUp5Application.class, args);
+    }
 
 }
